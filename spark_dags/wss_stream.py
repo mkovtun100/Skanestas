c8b9f2360444de3773ce0862a5449efb1f163478
Mikhail Kovtun <mkovtun@bostonsd.ru>
2023-08-17 13:55:38 +0300

    Final version of the uploader code.

--- spark_dags/wss_stream.py
+++ spark_dags/wss_stream.py
@@ -1,20 +1,41 @@
-from pyspark.sql import SparkSession
-import os
-from pyspark.sql.functions import *
+import json
+from websocket import create_connection
+from time import time
 
-spark = SparkSession \
-    .builder \
-    .appName("StructuredNetworkWordCount") \
-    .getOrCreate()
+N = 10
+ws_recv = create_connection("wss://test-ws.skns.dev/raw-messages")
 
-df = spark.readStream \
-      .format("socket") \
-      .option("host","test-ws.skns.dev/raw-messages") \
-      .option("port","80") \
-      .load()
 
-result = df.writeStream \
-  .format("console") \
-  .start()
+def message_handler(message_window):
+    time_max: float = 100.0
+    messages = []
+    counter_of_messages = 0
+    while True:
+        result = ws_recv.recv()
+        json_result = json.loads(result)
+        messages.append(json_result)
+        counter_of_messages += 1
+        if counter_of_messages == message_window:
+            # sorting of the messages' list
+            messages.sort(key=lambda x: x["id"], reverse=True)
+            '''
+            for i in range(N):
+                print(messages[i]["id"])
+            print("\n")
+            '''
+            # sending results with showing the sending time
+            time_initial = time()
+            ws_send = create_connection("wss://test-ws.skns.dev/ordered-messages/kovtun")
+            while len(messages) > 0:
+                ws_send.send(json.dumps(messages.pop()))
+            ws_send.close()
+            time_diff = time() - time_initial
+            if time_diff < time_max:
+                time_max = time_diff
+                print(time_diff)  # showing the minimum time taken to send all N ordered messages
 
-result.awaitTermination()
+            counter_of_messages = 0
+
+
+if __name__ == '__main__':
+    message_handler(N)
